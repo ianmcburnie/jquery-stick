4feb1f1d7f694ae48c898f2567bc8c5825747abd
Ian McBurnie <imcburnie@ebay.com>
2016-05-05 00:03:10 -0700

    Updated to support absolutely positioned elements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jquery-stick
 
-jQuery plugin that will stick a fixed position element next to any other page element.
+jQuery plugin that will stick absolutely positioned or fixed positioned elements relatively to another element
 
 This plugin is in early experimental status.
 
--- jquery.stick.js
+++ jquery.stick.js
@@ -1,46 +1,74 @@
 /**
-* @file jQuery plugin that sticks the given fixed position element to any another element
+* @file jQuery plugin that will stick an absolutely positioned or fixed positioned elements relatively to ancestor or viewport respectively.
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.0.1
+* @version 0.0.2
 * @requires jquery
 */
 (function($) {
-    function alignTop($el, fixedBounds, options) {
-        $el.css('bottom', window.innerHeight - fixedBounds.top - options.offsetTop + 'px');
+    function alignTop($el, bounds, offset, isFixedPosition) {
+        if (isFixedPosition) {
+            $el.css('bottom', window.innerHeight - bounds.top - offset + 'px');
+        } else {
+            $el.css('top', 0 - ($el.outerHeight()) + offset + 'px');
+        }
     }
 
-    function alignBottom($el, fixedBounds, options) {
-        $el.css('top', fixedBounds.bottom + options.offsetTop + 'px');
+    function alignBottom($el, bounds, offset) {
+        $el.css('top', bounds.bottom + offset + 'px');
     }
 
-    function alignVerticalBaseline($el, fixedBounds, options) {
-        $el.css('bottom', window.innerHeight - fixedBounds.bottom - options.offsetTop + 'px');
+    function alignVerticalStart($el, bounds, offset) {
+        $el.css('top', bounds.top + offset + 'px');
     }
 
-    function alignMiddle($el, fixedBounds, options) {
-        var targetDeltaHeight = fixedBounds.height - $el.outerHeight();
-        $el.css('top', fixedBounds.top + (targetDeltaHeight / 2) + options.offsetTop + 'px');
+    function alignVerticalEnd($el, bounds, offset, isFixedPosition) {
+        if (isFixedPosition) {
+            $el.css('bottom', window.innerHeight - bounds.bottom - offset + 'px');
+        } else {
+            $el.css('top', bounds.bottom - $el.outerHeight() - offset + 'px');
+        }
     }
 
-    function alignLeft($el, fixedBounds, options) {
-        $el.css('right', window.innerWidth - fixedBounds.left - options.offsetLeft + 'px');
+    function alignMiddle($el, bounds, offset) {
+        var targetDeltaHeight = bounds.height - $el.outerHeight();
+        $el.css('top', bounds.top + (targetDeltaHeight / 2) + offset + 'px');
     }
 
-    function alignHorizontalBaseline($el, fixedBounds, options) {
-        $el.css('left', fixedBounds.left + options.offsetLeft + 'px');
+    function alignLeft($el, bounds, offset, isFixedPosition) {
+        if (isFixedPosition) {
+            $el.css('right', window.innerWidth - bounds.left - offset + 'px');
+        } else {
+            $el.css('left', bounds.left - ($el.outerWidth()) + offset + 'px');
+        }
     }
 
-    function alignRight($el, fixedBounds, options) {
-        $el.css('left', fixedBounds.right + options.offsetLeft + 'px');
+    function alignHorizontalStart($el, bounds, offset, isFixedPosition) {
+        if (isFixedPosition) {
+            $el.css('left', bounds.left + offset + 'px');
+        } else {
+            $el.css('left', 0 + offset + 'px');
+        }
     }
 
-    function alignCenter($el, fixedBounds, options) {
-        var targetDeltaWidth = fixedBounds.width - $el.outerWidth();
-        $el.css('left', fixedBounds.left + (targetDeltaWidth / 2) + options.offsetLeft + 'px');
+    function alignHorizontalEnd($el, bounds, offset, isFixedPosition) {
+        if (isFixedPosition) {
+            $el.css('right', window.innerWidth - bounds.right + offset + 'px');
+        } else {
+            $el.css('left', bounds.right - $el.outerWidth() + offset + 'px');
+        }
+    }
+
+    function alignRight($el, bounds, offset) {
+        $el.css('left', bounds.right + offset + 'px');
+    }
+
+    function alignCenter($el, bounds, offset) {
+        var targetDeltaWidth = bounds.width - $el.outerWidth();
+        $el.css('left', bounds.left + (targetDeltaWidth / 2) + offset + 'px');
     }
 
     /**
-    * jQuery plugin that sticks the given fixed position element to any another element
+    * jQuery plugin that will stick an absolutely positioned or fixed positioned elements relatively to ancestor or viewport respectively
     *
     * @method "jQuery.fn.stick"
     * @todo Allow fixed bounds to be passed in if we want to avoid the calculation (e.g. on scroll)
@@ -58,10 +86,11 @@
             alignX: 0,
             alignY: 0,
             offsetTop: 0,
-            offsetLeft: 0
+            offsetLeft: 0,
+            isFixedPosition: false
         }, options);
 
-        var fixedBounds = $.fn.stick.getElementsFixedBounds(targetEl);
+        var bounds = (options.isFixedPosition === true) ? $.fn.stick.getElementsFixedBounds(targetEl) : $.fn.stick.getElementsBounds(targetEl);
 
         return this.each(function onEach() {
             var $this = $(this);
@@ -69,33 +98,49 @@
             switch (options.alignY) {
                 case 0:
                 case "top":
-                    alignTop($this, fixedBounds, options); break;
+                    alignTop($this, bounds, options.offsetTop, options.isFixedPosition);
+                    break;
                 case 1:
-                case "baseline":
-                    alignVerticalBaseline($this, fixedBounds, options); break;
+                case "start":
+                    alignVerticalStart($this, bounds, options.offsetTop);
+                    break;
                 case 2:
+                case "end":
+                    alignVerticalEnd($this, bounds, options.offsetTop, options.isFixedPosition);
+                    break;
+                case 3:
                 case "bottom":
                 default:
-                    alignBottom($this, fixedBounds, options); break;
-                case 3:
+                    alignBottom($this, bounds, options.offsetTop);
+                    break;
+                case 4:
                 case "middle":
-                    alignMiddle($this, fixedBounds, options); break;
+                    alignMiddle($this, bounds, options.offsetTop);
+                    break;
             }
 
             switch (options.alignX) {
                 case 0:
                 case "left":
-                    alignLeft($this, fixedBounds, options); break;
+                    alignLeft($this, bounds, options.offsetLeft, options.isFixedPosition);
+                    break;
                 case 1:
-                case "baseline":
+                case "start":
                 default:
-                    alignHorizontalBaseline($this, fixedBounds, options); break;
+                    alignHorizontalStart($this, bounds, options.offsetLeft, options.isFixedPosition);
+                    break;
                 case 2:
-                case "right":
-                    alignRight($this, fixedBounds, options); break;
+                case "end":
+                    alignHorizontalEnd($this, bounds, options.offsetLeft, options.isFixedPosition);
+                    break;
                 case 3:
+                case "right":
+                    alignRight($this, bounds, options.offsetLeft);
+                    break;
+                case 4:
                 case "center":
-                    alignCenter($this, fixedBounds, options); break;
+                    alignCenter($this, bounds, options.offsetLeft);
+                    break;
             }
         });
     };
@@ -122,6 +167,24 @@ $.fn.stick.getElementsFixedBounds = function(el) {
         right: (offset.left - scrollLeft) + outerWidth,
         middle: (offset.top - scrollTop) + (outerHeight / 2),
         center: (offset.left - scrollLeft) + (outerWidth / 2),
+        width: outerWidth,
+        height: outerHeight
+    };
+};
+
+$.fn.stick.getElementsBounds = function(el) {
+    var $el = $(el);
+    var offset = $el.offset();
+    var outerHeight = $el.outerHeight();
+    var outerWidth = $el.outerWidth();
+
+    return {
+        top: 0,
+        left: 0,
+        bottom: 0 + outerHeight,
+        right: 0 + outerWidth,
+        middle: 0 + (outerHeight / 2),
+        center: 0 + (outerWidth / 2),
         width: outerWidth,
         height: outerHeight
     };
